3d38d400db3ec0e3765ea71cd4d0886e64994a3b
blue-beaker <yyy5566778899@126.com>
2025-12-11 16:03:16 +0800

    readme, mod compat preparation

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 A mod to make intuitive in-game tutorials for modpacks and mod players.  
 
 Features:
-- Structure view by steps
+- Structure view by steps  
 - UI view  
+- Uses CraftTweaker API to manage entries and pages  
 
 TODO:
-- Informative text/item/image components in the view
+- Informative text/item/image components in the view  
+
+Notes:
+- Certain TileEntities from some mods may not render properly in this mod's structure view.  
--- src/main/java/io/bluebeaker/legacyponder/world/handler/EventHandlerFMP.java
+++ src/main/java/io/bluebeaker/legacyponder/world/handler/EventHandlerFMP.java
@@ -1,22 +1,17 @@
 package io.bluebeaker.legacyponder.world.handler;
 
-import codechicken.multipart.TileMultipart;
-import io.bluebeaker.legacyponder.structure.events.StructureTileEvent;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
 public class EventHandlerFMP {
-    @SubscribeEvent
-    public void onTileEntityLoad(StructureTileEvent.AfterLoad event){
-        TileEntity tile = event.tileEntity;
-        if(tile instanceof TileMultipart){
-        }
-    }
-    @SubscribeEvent
-    public void onTileEntityRender(StructureTileEvent.Render event){
-        TileEntity tile = event.tileEntity;
-        if(tile instanceof TileMultipart){
-//            LegacyPonder.getLogger().info("tile:{}",tile);
-        }
-    }
+//    @SubscribeEvent
+//    public void onTileEntityLoad(StructureTileEvent.AfterLoad event){
+//        TileEntity tile = event.tileEntity;
+//        if(tile instanceof TileMultipart){
+//        }
+//    }
+//    @SubscribeEvent
+//    public void onTileEntityRender(StructureTileEvent.Render event){
+//        TileEntity tile = event.tileEntity;
+//        if(tile instanceof TileMultipart){
+////            LegacyPonder.getLogger().info("tile:{}",tile);
+//        }
+//    }
 }
