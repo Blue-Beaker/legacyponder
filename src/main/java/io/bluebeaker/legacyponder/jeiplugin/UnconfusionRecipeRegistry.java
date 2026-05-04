92a94bd8200daec83c735d8396e3ac6adddf3bd8
blue-beaker <yyy5566778899@126.com>
2026-05-04 22:40:49 +0800

    Show title in JEI wrapper

--- src/main/java/io/bluebeaker/legacyponder/jeiplugin/UnconfusionRecipeRegistry.java
+++ src/main/java/io/bluebeaker/legacyponder/jeiplugin/UnconfusionRecipeRegistry.java
@@ -17,15 +17,15 @@ public class UnconfusionRecipeRegistry {
         List<UnconfusionRecipeWrapper> recipes = new ArrayList<>();
         for(String id: ManualRegistry.getEntries().keySet()){
             Entry entry = ManualRegistry.getEntries().get(id);
-            addWrapper(jeiHelpers, id, entry.summary, entry, recipes);
+            addWrapper(jeiHelpers, id, entry.title, entry.summary, entry, recipes);
         }
         return recipes;
     }
 
-    private static void addWrapper(IJeiHelpers jeiHelpers, String key, String summary, Entry internal, List<UnconfusionRecipeWrapper> recipes) {
+    private static void addWrapper(IJeiHelpers jeiHelpers, String key, String title, String summary, Entry internal, List<UnconfusionRecipeWrapper> recipes) {
         FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
 
-        List<String> lines = RenderUtils.listFormattedStringToWidth(fr,I18n.format(summary), UnconfusionRecipeCategory.recipeWidth-8);
+        List<String> lines = RenderUtils.listFormattedStringToWidth(fr,I18n.format(title)+"\n"+I18n.format(summary), UnconfusionRecipeCategory.recipeWidth-8);
         // Compute max lines
         int maxHeight = UnconfusionRecipeCategory.recipeHeight - 2 - 18 - ((internal.getFluids().size() + internal.getItems().size() - 1) / 9 * 18);
         if(!internal.getPages().isEmpty()) maxHeight=maxHeight-22;
