6b767d57dbf8849403f98d0185ed2323104c15bf
blue-beaker <yyy5566778899@126.com>
2026-07-21 21:01:38 +0800

    Clean and add error handler to BC event handlers

--- src/main/java/io/bluebeaker/legacyponder/compat/EventHandlerBC.java
+++ src/main/java/io/bluebeaker/legacyponder/compat/EventHandlerBC.java
@@ -3,6 +3,7 @@ package io.bluebeaker.legacyponder.compat;
 import buildcraft.lib.marker.MarkerConnection;
 import buildcraft.lib.tile.TileBC_Neptune;
 import buildcraft.lib.tile.TileMarker;
+import io.bluebeaker.legacyponder.LegacyPonder;
 import io.bluebeaker.legacyponder.compat.buildcraft.LaserConnectionsHandler;
 import io.bluebeaker.legacyponder.render.StructureRenderManager;
 import io.bluebeaker.legacyponder.structure.events.StructureCaptureEvent;
@@ -32,30 +33,29 @@ public class EventHandlerBC {
     public void onCapture(StructureCaptureEvent event){
         World world = event.world;
 
-        Set<MarkerConnection<?>> connections = new HashSet<>();
+        try {
 
-        for (BlockPos pos : BlockPos.getAllInBox(event.minPoint, event.maxPoint)) {
-            TileEntity tile = world.getTileEntity(pos);
-            if(tile instanceof TileMarker){
-                MarkerConnection<?> currentConnection = ((TileMarker<?>) tile).getCurrentConnection();
-                if(currentConnection != null){
-                    connections.add(currentConnection);
+            Set<MarkerConnection<?>> connections = new HashSet<>();
+
+            for (BlockPos pos : BlockPos.getAllInBox(event.minPoint, event.maxPoint)) {
+                TileEntity tile = world.getTileEntity(pos);
+                if (tile instanceof TileMarker) {
+                    MarkerConnection<?> currentConnection = ((TileMarker<?>) tile).getCurrentConnection();
+                    if (currentConnection != null) {
+                        connections.add(currentConnection);
+                    }
                 }
             }
-        }
 
-        NBTTagCompound bcData = new NBTTagCompound();
+            NBTTagCompound bcData = new NBTTagCompound();
 
-        NBTTagCompound markerData = LaserConnectionsHandler.dumpConnections(connections);
+            NBTTagCompound markerData = LaserConnectionsHandler.dumpConnections(connections);
 
-        bcData.setTag("markerConnections", markerData);
+            bcData.setTag("markerConnections", markerData);
 
-        event.extraData.setTag("buildcraft",bcData);
-    }
-
-    @SubscribeEvent
-    public void onTileLoad(StructureTileEvent.Load event){
-        if(event.tileEntity instanceof TileMarker) {
+            event.extraData.setTag("buildcraft", bcData);
+        }catch (Exception e){
+            LegacyPonder.getLogger().error("Error capturing BC extra data: {}",e.getMessage());
         }
     }
 
@@ -72,7 +72,11 @@ public class EventHandlerBC {
         GlStateManager.translate(-delta.x, -delta.y, -delta.z);
         RenderHelper.disableStandardItemLighting();
 
-        LaserConnectionsHandler.renderConnections(markerData);
+        try {
+            LaserConnectionsHandler.renderConnections(markerData);
+        }catch (Exception e){
+            LegacyPonder.getLogger().error("Error rendering BC extras: {}",e.getMessage());
+        }
 
         RenderHelper.enableStandardItemLighting();
         GlStateManager.translate(delta.x, delta.y, delta.z);
